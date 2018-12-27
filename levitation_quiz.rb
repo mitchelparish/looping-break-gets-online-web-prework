78300923d5f5194ce327b2a33e762159f340caf2
Mitchel Parish <mitchelparish@gmail.com>
2018-12-27 19:04:43 +0000

    Automatically backed up by Learn

--- levitation_quiz.rb
+++ levitation_quiz.rb
@@ -1,6 +1,13 @@
 
 def levitation_quiz
-	#your code here
+	loop do
+	  puts "What is the spell that enacts leviation?"
+    answer = gets.chomp
+    break if answer == "Wingardium Leviosa"
+  end
+  puts "You passed the quiz!"
+	end
+  
 end
 
 
